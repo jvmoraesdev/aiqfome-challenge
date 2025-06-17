f56239bd9b0ccc6b74c1ecc1065357f32336a88e
José Vinícius de Moraes <moraesjosev@gmail.com>
2025-06-16 23:39:28 -0300

    chore(config): :wrench: add prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      aiqfome
-    </div>
-  );
+  return <div className="flex min-h-screen flex-col items-center justify-center">aiqfome</div>;
 }
