34fdb161fb0b20a5c520659bc1bd192ab3c7807d
José Vinícius de Moraes <moraesjosev@gmail.com>
2025-06-20 10:11:32 -0300

    feat(ui): :lipstick: add color file and update globals css

---
+++ components/ui/Colors.ts
@@ -0,0 +1,13 @@
+const COLORS = {
+  background: '#ffffff',
+  foreground: '#f5f6f9',
+  primary: '#7b1fa2',
+  success: '#027A7A',
+  'border-light': '#eef0f5',
+  'text-light-primary': '#ffffff',
+  'text-light-secondary': '#eecffc',
+  'text-dark-primary': '#393a3c',
+  'text-dark-secondary': '#6d6f73'
+};
+
+export default COLORS;
