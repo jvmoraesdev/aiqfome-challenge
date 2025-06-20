ab3afb7b142447e75f35bbd84ef11f11bcec7da2
José Vinícius de Moraes <moraesjosev@gmail.com>
2025-06-20 10:12:38 -0300

    fix(ui): :bug: add cursor pointer to button

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -1,16 +1,16 @@
 'use client';
 
-import clsx from 'clsx';
+import classNames from 'classnames';
 import React, { ReactNode } from 'react';
 
 interface IButton {
   children: ReactNode;
-  action: () => void;
+  action?: () => void;
   className?: string;
 }
 
 const Button = ({ children, action, className }: IButton) => {
-  const classes = clsx('flex', className);
+  const classes = classNames(`flex ${action ? 'cursor-pointer' : 'cursor-default'}`, className);
 
   return (
     <button className={classes} onClick={action}>
