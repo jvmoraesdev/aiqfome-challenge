d903930e59590a0d674607518a026807e5781fbd
José Vinícius de Moraes <moraesjosev@gmail.com>
2025-06-20 10:13:24 -0300

    feat(ui): :sparkles: add text component

---
+++ components/ui/Text.tsx
@@ -0,0 +1,21 @@
+import classNames from 'classnames';
+import React, { ReactNode } from 'react';
+
+interface IText {
+  children: ReactNode;
+  className?: string;
+  size?: 'h1' | 'h2' | 'h3' | 'h4' | '';
+}
+
+const Text = ({ children, size, className }: IText) => {
+  const classes = classNames('', className);
+
+  if (size === 'h1') return <h1 className={classes}>{children}</h1>;
+  if (size === 'h2') return <h2 className={classes}>{children}</h2>;
+  if (size === 'h3') return <h3 className={classes}>{children}</h3>;
+  if (size === 'h4') return <h4 className={classes}>{children}</h4>;
+
+  return <span className={classes}>{children}</span>;
+};
+
+export default Text;
