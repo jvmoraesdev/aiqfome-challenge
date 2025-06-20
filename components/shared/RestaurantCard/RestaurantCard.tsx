8d4444cb5765e2d5a7c21aae1c4f6e1b30294c48
José Vinícius de Moraes <moraesjosev@gmail.com>
2025-06-20 10:15:33 -0300

    feat(ui): :sparkles: add restaurant card component

---
+++ components/shared/RestaurantCard/RestaurantCard.tsx
@@ -0,0 +1,41 @@
+import Image from 'next/image';
+import React from 'react';
+
+import Card from '@/components/ui/Card';
+import Text from '@/components/ui/Text';
+
+import DeliveryTag from '../DeliveryTag';
+import RestaurantRating from '../RestaurantRating';
+
+interface IRestaurantCard {
+  image: string;
+  name: string;
+  deliveryFee: number;
+  rating: number;
+  disabled?: boolean;
+}
+
+const RestaurantCard = ({ image, name, deliveryFee, rating, disabled }: IRestaurantCard) => {
+  return (
+    <Card>
+      <div className="flex h-[72px]">
+        <Image
+          src={image}
+          alt={name}
+          width={30}
+          height={30}
+          className={`${disabled ? 'opacity-45' : ''} size-[72px]`}
+        />
+        <div className="m-[12px] flex h-[50px] flex-col justify-between">
+          <Text className="text-dark-primary text-[16px]">{name}</Text>
+          <div className="flex items-center justify-start gap-[4px]">
+            <DeliveryTag fee={deliveryFee} isGhostIcon />
+            <RestaurantRating isComplete={false} rate={rating} />
+          </div>
+        </div>
+      </div>
+    </Card>
+  );
+};
+
+export default RestaurantCard;
