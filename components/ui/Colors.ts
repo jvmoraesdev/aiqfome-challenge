1cb2fff95062b7233d9ca279ce16e7bcef439ea9
José Vinícius de Moraes <moraesjosev@gmail.com>
2025-06-20 23:51:02 -0300

    feat(ui): :lipstick: add colors file

--- components/ui/Colors.ts
+++ components/ui/Colors.ts
@@ -1,11 +1,15 @@
 const COLORS = {
   background: '#ffffff',
   foreground: '#f5f6f9',
+  'foreground-secondary': '#eef0f5',
   primary: '#7b1fa2',
   success: '#027A7A',
+  'success-secondary': '#02a117',
+  'success-background': '#f2fafa',
   'border-light': '#eef0f5',
   'text-light-primary': '#ffffff',
   'text-light-secondary': '#eecffc',
+  'text-dark-contrast': '#202326',
   'text-dark-primary': '#393a3c',
   'text-dark-secondary': '#6d6f73'
 };
