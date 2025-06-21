4e7ccfbca8e01d5a3d718745764c8a0657eb2525
José Vinícius de Moraes <moraesjosev@gmail.com>
2025-06-21 16:48:37 -0300

    feat(ui): :sparkles: add product options component

---
+++ components/shared/ProductOption/ProductOption.tsx
@@ -0,0 +1,96 @@
+'use client';
+
+import React, { useEffect, useState } from 'react';
+
+import Card from '@/components/ui/Card';
+import Text from '@/components/ui/Text';
+import { ISelectedOptions } from '@/interfaces/general.interface';
+import { IProductOption } from '@/interfaces/product.interface';
+
+import ChecklistOptions from './ChecklistOptions';
+import MultipleOptions from './MultipleOptions';
+import RadioButtonOption from './RadioButtonOptions';
+
+interface IProductOptionComponent {
+  productOption: IProductOption;
+}
+
+const ProductOption = ({ productOption }: IProductOptionComponent) => {
+  const { name, minQuantity, maxQuantity, items } = productOption;
+  let type: string = 'checklist';
+  if (minQuantity === 1 && maxQuantity === 1) {
+    type = 'radio';
+  }
+  if (minQuantity === 0 && maxQuantity === 0) {
+    type = 'multiple';
+  }
+
+  const options = items.map((item) => ({
+    label: item.name,
+    value: item.id,
+    price: item.price,
+    promotionPrice: item.promotionPrice
+  }));
+
+  const [selectedValues, setSelectedValues] = useState<ISelectedOptions[]>([]);
+
+  useEffect(() => {
+    console.log(selectedValues);
+  }, [selectedValues]);
+
+  return (
+    <div className="bg-background flex flex-col gap-[16px] px-[16px] py-[16px]">
+      <div className="flex justify-between">
+        <div className="flex flex-col gap-[2px]">
+          <Text className="text-dark-contrast text-[16px]">{name}</Text>
+
+          {type === 'checklist' && minQuantity === 0 && (
+            <Text className="text-dark-secondary text-[12px]">{`escolha até ${maxQuantity}`}</Text>
+          )}
+          {type === 'checklist' && minQuantity !== 0 && (
+            <Text className="text-dark-secondary text-[12px]">{`escolha de ${minQuantity} a ${maxQuantity}`}</Text>
+          )}
+          {type === 'radio' && <Text className="text-dark-secondary text-[12px]">escolha 1</Text>}
+          {type === 'multiple' && (
+            <Text className="text-dark-secondary text-[12px]">escolha quantos quiser</Text>
+          )}
+        </div>
+
+        {minQuantity >= 1 && (
+          <Card className="text-background bg-dark-primary flex h-[28px] items-center justify-center rounded-[4px] p-[8px]">
+            <Text className="text-background text-[12px]">obrigatório</Text>
+          </Card>
+        )}
+      </div>
+
+      <div className="flex flex-col gap-[12px]">
+        {type === 'checklist' && (
+          <ChecklistOptions
+            options={options}
+            selectedValues={selectedValues}
+            onChange={setSelectedValues}
+            maxSelection={maxQuantity}
+          />
+        )}
+
+        {type === 'radio' && (
+          <RadioButtonOption
+            options={options}
+            selectedValue={selectedValues[0]?.value}
+            onChange={setSelectedValues}
+          />
+        )}
+
+        {type === 'multiple' && (
+          <MultipleOptions
+            options={options}
+            selectedValues={selectedValues}
+            onChange={setSelectedValues}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ProductOption;
